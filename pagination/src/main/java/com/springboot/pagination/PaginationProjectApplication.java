cc2ddf5e4962ff224d1eef211f8b5f625536034b
Shiva-indrakanti <indrakanti.98@gmail.com>
2025-11-05 12:04:52 +0530

    commit-01: project setup

---
+++ pagination/src/main/java/com/springboot/pagination/PaginationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.pagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationProjectApplication.class, args);
+	}
+
+}
